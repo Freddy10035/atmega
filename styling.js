7314cae6a8b491c0e8bb274c0fe740375a374983
Andridge Munene <andridgemunene@Andridges-MacBook-Pro.local>
2022-11-15 13:34:02 +0300

    message101257554

--- styling.js
+++ styling.js
@@ -1,2 +1,10 @@
 var laughter = "hahaha";
 console.log(laughter);
+(function () {
+    var btn = document.getElementsByClassName("feedback-body__submit")[0];
+    btn.onclick = function(e) {
+        e.preventDefault()
+        var laughter = "hahaha";
+        console.log(laughter);
+    }
+ })()
